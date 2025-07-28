aea171a082276c9c235c2270aabcb52283430a3c
Priyank Vasoya <priyankvasoya7@gmail.com>
2025-07-28 17:39:32 +1000

    TASK-25 Completed

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -32,7 +32,7 @@ const updateTask = async (req, res) => {
         task.deadline = deadline || task.deadline;
 
         const updatedTask = await task.save();
-        //Display the updated task list
+        //Display the updated task list 
         res.json(updatedTask);
 
     } catch (error) {
